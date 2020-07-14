3e6a3c4b6839ad212363a36521f423335c4f0c0b
Jefferson Brandão <jeffersonbraster@gmail.com>
2020-07-13 23:51:43 -0300

    Corrigindo os imports

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 import multer from 'multer';
-import uploadConfig from '../config/upload';
-import CreateUserServices from '../services/CreateUserServices';
+import uploadConfig from '@config/upload';
+import CreateUserServices from '@modules/users/services/CreateUserServices';
+import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
-import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -2,10 +2,9 @@ import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
-import authconfig from '../config/auth';
-import User from '../models/User';
-
-import AppError from '../errors/AppError';
+import authconfig from '@config/auth';
+import AppError from '@shared/errors/AppError';
+import User from '../infra/typeorm/entities/User';
 
 interface Request {
   email: string;
