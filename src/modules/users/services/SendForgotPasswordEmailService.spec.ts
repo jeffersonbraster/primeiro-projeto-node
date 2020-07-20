9df0d44f9289d49953a217700994fdcc1a669100
Jefferson Brandão <jeffersonbraster@gmail.com>
2020-07-20 17:59:42 -0300

    Realizando teste caso nao exista email cadastrado

--- src/modules/users/services/SendForgotPasswordEmailService.spec.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -1,4 +1,4 @@
-// import AppError from '@shared/errors/AppError';
+import AppError from '@shared/errors/AppError';
 import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
@@ -26,5 +26,21 @@ describe('SendForgotPasswordEmail', () => {
     });
 
     expect(sendMail).toHaveBeenCalled();
+  });
+
+  it('should be not able to recover a non-existing user password', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeMailProvider = new FakeMailProvider();
+
+    const sendForgotPasswordEmail = new SendForgotPasswordEmailService(
+      fakeUsersRepository,
+      fakeMailProvider,
+    );
+
+    await expect(
+      sendForgotPasswordEmail.execute({
+        email: 'jejezinho@gmail.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,8 +1,10 @@
 import 'reflect-metadata';
+import AppError from '@shared/errors/AppError';
 import { injectable, inject } from 'tsyringe';
-// import AppError from '@shared/errors/AppError';
+
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
+import UsersRepository from '../infra/typeorm/repositories/UsersRepository';
 
 // import User from '../infra/typeorm/entities/User';
 
@@ -20,6 +22,12 @@ export default class SendForgotPasswordEmailService {
   ) {}
 
   public async execute({ email }: IRequest): Promise<void> {
+    const checkUserExist = await this.usersRepository.findByEmail(email);
+
+    if (!checkUserExist) {
+      throw new AppError('User not existing');
+    }
+
     this.mailProvider.sendMail(
       email,
       'Pedido de recuperação de senha recebida',
