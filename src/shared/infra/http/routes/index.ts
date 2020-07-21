75b4976cd459d3c7bc3030c64d412602a2311eb6
Jefferson Brandão <jeffersonbraster@gmail.com>
2020-07-20 22:50:20 -0300

    Criando rotas, controller, repositorio, e migrations de tokens do reset password

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionRoute from '@modules/users/infra/http/routes/sessions.routes';
+import passwordRoute from '@modules/users/infra/http/routes/password.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionRoute);
+routes.use('/password', passwordRoute);
 
 export default routes;
