a863ce95f471d896d9c86a1e396f1c4a7e1b9964
Jefferson Brandão <jeffersonbraster@gmail.com>
2020-07-23 22:19:15 -0300

    Rotas e controllers do provider de day e month

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,12 +1,24 @@
 import { Router } from 'express';
 import ensureAuthenticaded from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProviderController from '../controllers/ProviderController';
+import ProviderMothAvailabilityController from '../controllers/ProviderMonthAvailabilityController';
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController';
 
 const providerRouter = Router();
 const providerController = new ProviderController();
+const providerMothAvailabilityController = new ProviderMothAvailabilityController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
 
 providerRouter.use(ensureAuthenticaded);
 
 providerRouter.get('/', providerController.index);
+providerRouter.get(
+  '/:provider_id/month-availability',
+  providerMothAvailabilityController.index,
+);
+providerRouter.get(
+  '/:provider_id/day-availability',
+  providerDayAvailabilityController.index,
+);
 
 export default providerRouter;
