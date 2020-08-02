3bb4b767275f2e7951f04f2d1c96c694858e82f9
Jefferson Brandão <jeffersonbraster@gmail.com>
2020-08-02 14:30:17 -0300

    Serializaçao no cache

--- src/modules/appointments/services/ListProvidersAppointmentsService.ts
+++ src/modules/appointments/services/ListProvidersAppointmentsService.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import { injectable, inject } from 'tsyringe';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 import Appointment from '../infra/typeorm/entities/Appointment';
 
 // import User from '@modules/users/infra/typeorm/entities/User';
@@ -46,12 +47,8 @@ export default class ListProviderAppointmentsService {
         },
       );
 
-      console.log('buscou do banco');
-
-      await this.cacheProvider.save(cacheKey, appointments);
+      await this.cacheProvider.save(cacheKey, classToClass(appointments));
     }
-
-    // await this.cacheProvider.save('asas', 'asasas');
 
     return appointments;
   }
