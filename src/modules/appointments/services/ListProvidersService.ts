581e8eab51b14cfa31e0274c0fbb717fc2d30b1d
Jefferson Brandão <jeffersonbraster@gmail.com>
2020-08-08 10:09:20 -0300

    Ajustando api

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -3,6 +3,7 @@ import { injectable, inject } from 'tsyringe';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import { classToClass } from 'class-transformer';
 
 interface IRequest {
   user_id: string;
@@ -22,6 +23,8 @@ export default class ListProvidersService {
       `providers-list: ${user_id}`,
     );
 
+    // let users;
+
     if (!users) {
       users = await this.usersRepository.findAllProviders({
         except_user_id: user_id,
@@ -29,7 +32,10 @@ export default class ListProvidersService {
 
       console.log('A query no banco foi feita!');
 
-      await this.cacheProvider.save(`providers-list: ${user_id}`, users);
+      await this.cacheProvider.save(
+        `providers-list: ${user_id}`,
+        classToClass(users),
+      );
     }
 
     return users;
